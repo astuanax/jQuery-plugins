18a6aff020a670b24580c6d6612bc373e1e60f60
Astuanax <len@astuanax.com>
2012-09-15 18:57:04 +0200

    Heigh-Width ration

--- stretch/stretch.js
+++ stretch/stretch.js
@@ -5,12 +5,28 @@
 		var options = $.extend({}, $.fn.stretch.defaultOptions, customOptions);
 		return this.each(function() {
 
-			var $this = $(this);
+			    var $this = $(this);
 
-			//wrap the img in forced block element
-			$this.wrap("<span style=\"display:block\" class='" + options.className + "' />");
-			$("span." + options.className).css("width", options.spanWidth).children('img').css("max-width", options.maximumWidth);
+				  // Remove existing CSS
+				// Remove HTML attributes
+				$this.css({"width":"auto", "height":"auto"}).removeAttr("width").removeAttr("height");
 
+				//wrap the img in forced block element
+				$this.wrap("<span class='"+options.className+"' />");
+
+				$( $this ).load(function(){
+						var width  = $this.width();
+						var height = $this.height();	
+				if(width > height) {
+					$("span."+options.className).css({'width' : options.spanWidth, 'display' : 'block'} );
+					$this.css({ 'max-width' : options.maximumWidth, 'max-height' : options.maximumWidth });						
+
+				}
+				if(height > width) {
+					$("span."+options.className).css({'height' : options.spanWidth, 'display' : 'block'} );
+					$this.css({ 'max-height' : 'auto', 'max-width' : options.maximumWidth });										
+				}
+			});
 			_debug([encodeURI($this.attr('src')), $this]);
 		});
 	};
@@ -44,3 +60,5 @@
 
 
 })(jQuery);
+
+
